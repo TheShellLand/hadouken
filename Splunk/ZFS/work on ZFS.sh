edcc6e2b58c557ad52a12c3dc76904e0bf571b8c
eric <naisanza@gmail.com>
2016-04-01 21:16:28 -0400

    Fix for Splunk to work on top of ZFS

--- Splunk/ZFS/work on ZFS.sh
+++ Splunk/ZFS/work on ZFS.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+#
+# Fix for using Splunk on top of ZFS
+# Because if it's not ZFS, why bother?
+#
+# Pulled from https://answers.splunk.com/answers/315033/will-there-be-zfs-support-on-linux-for-splunk-ente.html#answer-317186
+# Thanks dude
+
 echo OPTIMISTIC_ABOUT_FILE_LOCKING = 1 >> /opt/splunk/etc/splunk-launch.conf
